09544d4ed1e2c70ecffb65b6f9b36b09bb855820
kamimi01 <awakaru0122@gmail.com>
2020-09-27 17:42:22 +0900

    add: ログイン画面が大体完成

--- ArticleCollection/View/Login/LoginViewController.swift
+++ ArticleCollection/View/Login/LoginViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var appTitleLabel: UILabel!
+    @IBOutlet weak var appTitleLowLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var attensionTextView: UITextView!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup() {
+        // 全体の設定
+        view.backgroundColor = UIColor.onion
+        
+        // アプリタイトルの設定
+        appTitleLabel.textColor = UIColor.driftWood
+        appTitleLowLabel.textColor = UIColor.driftWood
+        
+        // ユーザー名入力欄の設定
+        usernameTextField.customizeTextField(.main, " ユーザー名")
+        
+        // 注意書きの設定
+        attensionTextView.customizeTextView(.main)
+        
+        // はじめるボタンの設定
+        nextButton.customizeButton(.mainActive, "はじめる→")
     }
-    */
-
+    
+    @IBAction func nextButtionTap(_ sender: Any) {
+        Transition.transitionDestination(self, "TabBar", .fullScreen)
+    }
 }
