d6556add8ac2f1aac38ea107ba9ba694058a650b
kamimi01 <awakaru0122@gmail.com>
2020-09-27 12:40:50 +0900

    Initial Commit

---
+++ ArticleCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleCollection
+//
+//  Created by Mika Urakawa on 2020/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
