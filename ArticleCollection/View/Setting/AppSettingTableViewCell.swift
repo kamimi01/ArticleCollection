071262ae20bd560e98da3ce245194d8e061294df
kamimi01 <awakaru0122@gmail.com>
2020-09-28 21:26:37 +0900

    add: テーブルの表示を追加

---
+++ ArticleCollection/View/Setting/AppSettingTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  AppSettingTableViewCell.swift
+//  ArticleCollection
+//
+//  Created by Mika Urakawa on 2020/09/27.
+//
+
+import UIKit
+
+class AppSettingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameNowLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup() {
+        usernameLabel.textColor = UIColor.gray
+        usernameNowLabel.textColor = UIColor.gray
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ArticleCollection/View/Setting/SettingViewController.swift
+++ ArticleCollection/View/Setting/SettingViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let sectionNames = ["アプリ設定"]
+    let appSettingKind = ["ユーザー名"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,30 @@ class SettingViewController: UIViewController {
         self.navigationController?.navigationBar.titleTextAttributes = [
             .foregroundColor: UIColor.gray
         ]
+        
+        // tableViewの設定
+        tableViewSetUp()
+    }
+    
+    private func tableViewSetUp() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // xibの登録
+        let nib = UINib(nibName: "AppSettingTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "AppSettingTableViewCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AppSettingTableViewCell", for: indexPath ) as! AppSettingTableViewCell
+        
+        cell.usernameLabel.text = appSettingKind[indexPath.row]
+        cell.usernameNowLabel.text = "Kamimi01"
+        return cell
     }
 
 }
