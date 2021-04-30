beb1f5361dbb134ef8d0f80d07cf819a76f160d0
kamimi01 <awakaru0122@gmail.com>
2021-05-01 01:21:37 +0900

    feat: お気に入り機能の実装中

---
+++ ArticleCollection/Model/ArticleStateManager.swift
@@ -0,0 +1,22 @@
+//
+//  ArticleState.swift
+//  ArticleCollection
+//
+//  Created by Mika Urakawa on 2021/04/30.
+//
+
+import Foundation
+
+//struct ArticleState {
+////    var isFavorite: Bool
+//    var isFavorite: [Bool] = []
+//}
+
+// シングルトンパターンを使用し、常に1つのインスタンスのみが存在することを保証する
+final public class ArticleStateManager {
+    public static let shared = ArticleStateManager()
+    private init() {}
+    
+    // お気に入りかどうかのフラグリスト
+    var favoriteStatusList: [Bool] = []
+}
