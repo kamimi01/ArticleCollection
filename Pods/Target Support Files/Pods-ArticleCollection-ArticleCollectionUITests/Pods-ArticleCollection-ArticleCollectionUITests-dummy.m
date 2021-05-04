e57c631e1beca88484030bb067a1a58ed689b90e
kamimi01 <47489629+kamimi01@users.noreply.github.com>
2021-05-04 20:01:37 +0900

    fix: Podsディレクトリの削除

--- Pods/Target Support Files/Pods-ArticleCollection-ArticleCollectionUITests/Pods-ArticleCollection-ArticleCollectionUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ArticleCollection_ArticleCollectionUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ArticleCollection_ArticleCollectionUITests
-@end
